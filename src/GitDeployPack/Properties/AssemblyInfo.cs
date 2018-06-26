a191d18630c39f4f1b852303038236f53e39273c
huangyu <huangyu@gaohangip.com>
2018-06-26 18:42:19 +0800

    modify assemblyInfo

--- src/GitDeployPack/Properties/AssemblyInfo.cs
+++ src/GitDeployPack/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("RxConsole")]
+[assembly: AssemblyTitle("GitDeployPack.Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RxConsole")]
+[assembly: AssemblyProduct("GitDeployPack.Console")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
