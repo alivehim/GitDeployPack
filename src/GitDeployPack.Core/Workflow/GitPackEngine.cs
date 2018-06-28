24381e4e5d994eef442ccdbb57f2fff13e1ea21b
huangyu <huangyu@gaohangip.com>
2018-06-28 20:13:06 +0800

    .....

--- src/GitDeployPack.Core/Workflow/GitPackEngine.cs
+++ src/GitDeployPack.Core/Workflow/GitPackEngine.cs
@@ -66,11 +66,11 @@ namespace GitDeployPack.Core
         public IDisposable Run()
         {
             Console.WriteLine("start....");
-            //if(GitCommandHelper.GetCurrentBranch(options.GitWorkPath)!=options.NewRepository)
-            //{
-            //    logger.Information($"branch error {options.NewRepository}  ");
-            //    return new CompositeDisposable();
-            //}
+            if (GitCommandHelper.GetCurrentBranch(options.GitWorkPath) != options.NewRepository)
+            {
+                logger.Information($"branch error {options.NewRepository}  ");
+                return new CompositeDisposable();
+            }
 
             //get root work directory
 
